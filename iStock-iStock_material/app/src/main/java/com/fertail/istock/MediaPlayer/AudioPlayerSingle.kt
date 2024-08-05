d90059b92c1a8532791ffe8a236ec15f2bb04248
balaandroid <balakrishnan@codasol.com>
2024-08-05 11:43:54 +0530

    first commit

---
+++ iStock-iStock_material/app/src/main/java/com/fertail/istock/MediaPlayer/AudioPlayerSingle.kt
@@ -0,0 +1,12 @@
+package com.fertail.istock.MediaPlayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fertail.istock.R
+
+class AudioPlayerSingle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio_player_single)
+    }
+}
